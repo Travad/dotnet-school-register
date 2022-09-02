c1bd2c5e495e44239483c068d40ae1dfd308399a
TravazStake <dtravaz@gmail.com>
2022-09-02 13:08:56 +0200

    Added model and API endpoint for single student details

--- SchoolRegister.Model2/CourseAttendeeDto.cs
+++
@@ -1,9 +0,0 @@
-namespace SchoolRegister.Model;
-
-public class CourseAttendeeDto
-{
-    public int CourseId { get; set; }
-    public CourseDto Course { get; set; }
-    public int AttendeeId { get; set; }
-    public AttendeeDto Attendee { get; set; }
-}
--- dotnet-school-register/Entities/Grades/GradeType.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnet_school_register.Entities.Grades;
-
-public enum GradeType
-{
-    Oral,
-    Written,
-    Exam
-}
